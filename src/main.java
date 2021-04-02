265f0ffc88ef69f7fad7a7a2333af50234985bf8
shadowk9 <69080521+shadowk9@users.noreply.github.com>
2021-04-02 17:22:34 +0530

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println("sum is");
+
+        for(int i=0;i<n; )
+        {
+            System.out.println(gfg.fib(i)+" ");
+            i++;
+        }
+    }
+}
